494c864f9b7601182562d448d61ad32e791689b5
RajMahara <zezinfreestyle3@hotmail.com>
2020-10-04 14:22:12 -0300

    directions

--- snake.py
+++ snake.py
@@ -1,9 +1,17 @@
 import pygame
 from pygame.locals import *
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake')
+
+snake = [(200,200),(210,200),(220,200)]
+my_direction = LEFT
 
 while True:
 
